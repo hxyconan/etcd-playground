2173df8e2ba8725c16cbe880af17f60c4ecb3660
Bob Huang <xiaoyu0571@gmail.com>
2023-09-21 10:54:54 +0000

    add a remove etcd member list script file
    also update the check member list script

--- remove-etcd-member-script.sh
+++ remove-etcd-member-script.sh
@@ -1,10 +1,9 @@
-
-## Check etcd members
+## Remove etcd members
 export ETCDCTL_API=3
 export HOST_1=192.168.33.21
 export HOST_2=192.168.33.22
 export HOST_3=192.168.33.23
 export ENDPOINTS=$HOST_1:2379,$HOST_2:2379,$HOST_3:2379
 
-etcdctl --endpoints=$ENDPOINTS member list
-
+MEMBER_ID=278c654c9a6dfd3b
+etcdctl --endpoints=$ENDPOINTS member remove ${MEMBER_ID}
